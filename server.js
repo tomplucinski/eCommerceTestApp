79deee555b5b8be4ca170ba2f83ad556ad20df9d
Tom Plucinski <tapluci1@gmail.com>
2021-02-18 22:45:36 -0600

    Integrated with billing api

--- server.js
+++ server.js
@@ -1,26 +1,31 @@
 require('isomorphic-fetch');
+const dotenv = require('dotenv');
+dotenv.config();
 const Koa = require('koa');
 const next = require('next');
 const { default: createShopifyAuth } = require('@shopify/koa-shopify-auth');
-const dotenv = require('dotenv');
 const { verifyRequest } = require('@shopify/koa-shopify-auth');
 const session = require('koa-session');
-
-dotenv.config();
 const { default: graphQLProxy } = require('@shopify/koa-shopify-graphql-proxy');
 const { ApiVersion } = require('@shopify/koa-shopify-graphql-proxy');
+const Router = require('koa-router');
+const { receiveWebhook, registerWebhook } = require('@shopify/koa-shopify-webhooks');
+const getSubscriptionUrl = require('./server/getSubscriptionUrl');
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-
-const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY } = process.env;
-console.log("HERE", SHOPIFY_API_KEY, SHOPIFY_API_SECRET_KEY)
+const {
+  SHOPIFY_API_SECRET_KEY,
+  SHOPIFY_API_KEY,
+  HOST,
+} = process.env;
 
 app.prepare().then(() => {
   const server = new Koa();
+  const router = new Router();
   server.use(session({ sameSite: 'none', secure: true }, server));
   server.keys = [SHOPIFY_API_SECRET_KEY];
 
@@ -29,23 +34,47 @@ app.prepare().then(() => {
       apiKey: SHOPIFY_API_KEY,
       secret: SHOPIFY_API_SECRET_KEY,
       scopes: ['read_products', 'write_products'],
-      afterAuth(ctx) {
-        const urlParams = new URLSearchParams(ctx.request.url);
-        const shop = urlParams.get('shop');
+      async afterAuth(ctx) {
+        const { shop, accessToken } = ctx.session;
+        ctx.cookies.set("shopOrigin", shop, {
+          httpOnly: false,
+          secure: true,
+          sameSite: 'none'
+        });
+        const registration = await registerWebhook({
+          address: `${HOST}/webhooks/products/create`,
+          topic: 'PRODUCTS_CREATE',
+          accessToken,
+          shop,
+          apiVersion: ApiVersion.October20
+        });
 
-        ctx.redirect(`/?shop=${shop}`);
-      },
-    }),
+        if (registration.success) {
+          console.log('Successfully registered webhook!');
+        } else {
+          console.log('Failed to register webhook', registration.result);
+        }
+        await getSubscriptionUrl(ctx, accessToken, shop);
+      }
+    })
   );
 
-  server.use(graphQLProxy({version: ApiVersion.October20}))
-  server.use(verifyRequest());
-  server.use(async (ctx) => {
+  const webhook = receiveWebhook({ secret: SHOPIFY_API_SECRET_KEY });
+
+  router.post('/webhooks/products/create', webhook, (ctx) => {
+    console.log('received webhook: ', ctx.state.webhook);
+  });
+
+  server.use(graphQLProxy({ version: ApiVersion.July20 }));
+
+  router.get('(.*)', verifyRequest(), async (ctx) => {
     await handle(ctx.req, ctx.res);
     ctx.respond = false;
     ctx.res.statusCode = 200;
-    return
   });
+
+  server.use(router.allowedMethods());
+  server.use(router.routes());
 
   server.listen(port, () => {
     console.log(`> Ready on http://localhost:${port}`);
---
+++ server/getSubscriptionUrl.js
@@ -0,0 +1,52 @@
+const getSubscriptionUrl = async (accessToken, shop, returnUrl = process.env.HOST) => {
+    const query = JSON.stringify({
+      query: `mutation {
+        appSubscriptionCreate(
+          name: "Super Duper Plan"
+          returnUrl: "${returnUrl}"
+          test: true
+          lineItems: [
+            {
+              plan: {
+                appUsagePricingDetails: {
+                  cappedAmount: { amount: 10, currencyCode: USD }
+                  terms: "$1 for 1000 emails"
+                }
+              }
+            }
+            {
+              plan: {
+                appRecurringPricingDetails: {
+                  price: { amount: 10, currencyCode: USD }
+                }
+              }
+            }
+          ]
+        )
+        {
+          userErrors {
+            field
+            message
+          }
+          confirmationUrl
+          appSubscription {
+            id
+          }
+        }
+      }`
+    });
+  
+    const response = await fetch(`https://${shop}/admin/api/2020-10/graphql.json`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        "X-Shopify-Access-Token": accessToken,
+      },
+      body: query
+    })
+  
+    const responseJson = await response.json();
+    return responseJson.data.appSubscriptionCreate.confirmationUrl;
+  };
+  
+  module.exports = getSubscriptionUrl;
